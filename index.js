82dc053d283156f0a47f86b4500ccbc06fde4ca4
cliuzy <97586447+cliuzy@users.noreply.github.com>
2022-10-17 08:44:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+ // $("button").on("click", function (event) {
+ //     $(".contact-4").hide(1000);
+ // });
+ //
+ $(".btn").on("click", function (event) {
+     $(".cont").toggle(1000);
+ });
+ //
+ // $("button").on("click", function (event) {
+ //     $(".contact-4").show(1000);
+ // });
+
+alert ('hello');
